44f59002f47d91a7f2215d27ce627d2049ee42d6
Darren Tsung <darren.tsung@gmail.com>
2020-10-04 10:37:03 -0700

    Add winner calculation

--- frontend/components/tictactoe.tsx
+++ frontend/components/tictactoe.tsx
@@ -26,6 +26,10 @@ class Board extends React.Component<{}, BoardState> {
     }
 
     handleClick(i: number) {
+        if (calculateWinner(this.state.squares) || this.state.squares[i]) {
+            return;
+        }
+
         const squares = this.state.squares.slice();
         squares[i] = this.state.xPlayersTurn ? 'X' : 'O';
         this.setState({
@@ -39,7 +43,13 @@ class Board extends React.Component<{}, BoardState> {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xPlayersTurn ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status: string;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xPlayersTurn ? 'X' : 'O');
+        };
 
         return (
             <div>
@@ -78,4 +88,24 @@ export class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calculateWinner(squares: string[]): string | null {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
